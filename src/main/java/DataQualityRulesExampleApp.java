3bb8d41f0c198b3976ff4931c38709578d3e3a03
gphilipp <gilles.philippart@gmail.com>
2023-10-16 22:37:20 +0200

    Add rules.executors prop

--- src/main/java/DataQualityRulesExampleApp.java
+++ src/main/java/DataQualityRulesExampleApp.java
@@ -1,3 +1,5 @@
+import io.confluent.kafka.schemaregistry.client.rest.exceptions.RestClientException;
+import io.confluent.kafka.serializers.AbstractKafkaSchemaSerDeConfig;
 import io.confluent.kafka.serializers.KafkaAvroSerializer;
 import org.apache.kafka.clients.producer.KafkaProducer;
 import org.apache.kafka.clients.producer.ProducerConfig;
@@ -12,12 +14,10 @@ import java.nio.file.Paths;
 import java.time.LocalDate;
 import java.time.Month;
 import java.util.Properties;
-import java.util.Scanner;
 
 public class DataQualityRulesExampleApp {
 
     private static final String TOPIC = "memberships";
-    private static final String USER_SCHEMA = "memberships.avsc";
 
 
     public static Properties loadConfig(final String configFile) throws IOException {
@@ -35,20 +35,53 @@ public class DataQualityRulesExampleApp {
         final Properties properties = loadConfig("client.properties");
         properties.put(ProducerConfig.KEY_SERIALIZER_CLASS_CONFIG, StringSerializer.class);
         properties.put(ProducerConfig.VALUE_SERIALIZER_CLASS_CONFIG, KafkaAvroSerializer.class);
+        properties.put(AbstractKafkaSchemaSerDeConfig.USE_LATEST_VERSION, true);
+        properties.put("rules.executors", "checkSsnLen");
+        properties.put("rule.executors.checkSsnLen.class", "io.confluent.kafka.schemaregistry.rules.cel.CelExecutor");
 
-        try (KafkaProducer<String, Membership> producer = new KafkaProducer<>(properties)) {
-            Membership membership = Membership.newBuilder()
-                    .setEmail("joe@doe.com")
-                    .setSsn("856-45-6789")
-                    .setStartDate(LocalDate.of(2023, Month.JANUARY, 1))
-                    .setEndDate(LocalDate.of(2023, Month.DECEMBER, 31))
-                    .build();
+        KafkaProducer<String, Membership> producer = new KafkaProducer<>(properties);
+        try {
+            // Can be produced ok
+
+            /*produceMembership(producer,
+                    makeMembership(
+                            LocalDate.of(2023, Month.JANUARY, 1),
+                            LocalDate.of(2023, Month.DECEMBER, 31),
+                            "joe@doe.com",
+                            "856-45-6789"
+                    )
+            )*/
+            ;
+
+            produceMembership(producer,
+                    makeMembership(
+                            LocalDate.of(2023, Month.JANUARY, 1),
+                            LocalDate.of(1970, Month.JANUARY, 1),
+                            "john.doe",
+                            "fizzbuzz"
+                    ));
+        } finally {
+            producer.close();
+        }
+    }
+
+    private static Membership makeMembership(LocalDate startDate, LocalDate endDate, String email, String ssn) {
+        return Membership.newBuilder()
+                .setEmail(email)
+                .setSsn(ssn)
+                .setStartDate(startDate)
+                .setEndDate(endDate)
+                .build();
+    }
+
+    private static void produceMembership(KafkaProducer<String, Membership> producer, Membership membership) {
+        try {
             ProducerRecord<String, Membership> record = new ProducerRecord<>(TOPIC, membership);
             producer.send(record, (recordMetadata, e) -> {
                 if (e == null) {
-                    System.out.println("Record written to offset " +
-                            recordMetadata.offset() + " timestamp " +
-                            recordMetadata.timestamp());
+                    System.out.println("Record written to partition " + recordMetadata.partition() +
+                            ", offset " + recordMetadata.offset() +
+                            ", timestamp " + recordMetadata.timestamp());
                 } else {
                     System.err.println("An error occurred");
                     e.printStackTrace(System.err);
@@ -59,11 +92,4 @@ public class DataQualityRulesExampleApp {
             e.printStackTrace();
         }
     }
-
-    private static String loadSchemaFromFile(String filepath) {
-        InputStream inputStream = DataQualityRulesExampleApp.class.getResourceAsStream(filepath);
-        Scanner s = new Scanner(inputStream).useDelimiter("\\A");
-        return s.hasNext() ? s.next() : "";
-    }
-
 }
