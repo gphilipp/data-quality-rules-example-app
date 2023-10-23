956559b6dfd2012aee474cb872d52e3a45dc8865
gphilipp <gilles.philippart@gmail.com>
2023-10-23 17:35:29 +0200

    Refactor

--- src/main/java/DataQualityRulesExampleApp.java
+++ src/main/java/DataQualityRulesExampleApp.java
@@ -1,4 +1,3 @@
-import io.confluent.kafka.schemaregistry.client.rest.exceptions.RestClientException;
 import io.confluent.kafka.serializers.AbstractKafkaSchemaSerDeConfig;
 import io.confluent.kafka.serializers.KafkaAvroSerializer;
 import org.apache.kafka.clients.producer.KafkaProducer;
@@ -32,50 +31,15 @@ public class DataQualityRulesExampleApp {
     }
 
     public static void main(String[] args) throws IOException {
-        final Properties properties = loadConfig("client.properties");
-        properties.put(ProducerConfig.KEY_SERIALIZER_CLASS_CONFIG, StringSerializer.class);
-        properties.put(ProducerConfig.VALUE_SERIALIZER_CLASS_CONFIG, KafkaAvroSerializer.class);
-        properties.put(AbstractKafkaSchemaSerDeConfig.USE_LATEST_VERSION, true);
-        properties.put("rules.executors", "checkSsnLen");
-        properties.put("rule.executors.checkSsnLen.class", "io.confluent.kafka.schemaregistry.rules.cel.CelExecutor");
+        final Properties properties = loadProps();
 
-        KafkaProducer<String, Membership> producer = new KafkaProducer<>(properties);
-        try {
-            // Can be produced ok
-
-            /*produceMembership(producer,
-                    makeMembership(
-                            LocalDate.of(2023, Month.JANUARY, 1),
-                            LocalDate.of(2023, Month.DECEMBER, 31),
-                            "joe@doe.com",
-                            "856-45-6789"
-                    )
-            )*/
-            ;
-
-            produceMembership(producer,
-                    makeMembership(
-                            LocalDate.of(2023, Month.JANUARY, 1),
-                            LocalDate.of(1970, Month.JANUARY, 1),
-                            "john.doe",
-                            "fizzbuzz"
-                    ));
-        } finally {
-            producer.close();
-        }
-    }
-
-    private static Membership makeMembership(LocalDate startDate, LocalDate endDate, String email, String ssn) {
-        return Membership.newBuilder()
-                .setEmail(email)
-                .setSsn(ssn)
-                .setStartDate(startDate)
-                .setEndDate(endDate)
-                .build();
-    }
-
-    private static void produceMembership(KafkaProducer<String, Membership> producer, Membership membership) {
-        try {
+        try (KafkaProducer<String, Membership> producer = new KafkaProducer<>(properties)) {
+            Membership membership = Membership.newBuilder()
+                    .setEmail("john.doe")
+                    .setSsn("fizzbuzz")
+                    .setStartDate(LocalDate.of(2023, Month.JANUARY, 1))
+                    .setEndDate(LocalDate.of(1970, Month.JANUARY, 1))
+                    .build();
             ProducerRecord<String, Membership> record = new ProducerRecord<>(TOPIC, membership);
             producer.send(record, (recordMetadata, e) -> {
                 if (e == null) {
@@ -83,13 +47,21 @@ public class DataQualityRulesExampleApp {
                             ", offset " + recordMetadata.offset() +
                             ", timestamp " + recordMetadata.timestamp());
                 } else {
-                    System.err.println("An error occurred");
                     e.printStackTrace(System.err);
                 }
             });
             producer.flush();
         } catch (Exception e) {
-            e.printStackTrace();
+            System.err.println("Unable to produce, cause: " + e.getCause().getMessage());
         }
+    }
+
+    private static Properties loadProps() throws IOException {
+        final Properties properties = loadConfig("client.properties");
+        properties.put(ProducerConfig.KEY_SERIALIZER_CLASS_CONFIG, StringSerializer.class);
+        properties.put(ProducerConfig.VALUE_SERIALIZER_CLASS_CONFIG, KafkaAvroSerializer.class);
+        properties.put(AbstractKafkaSchemaSerDeConfig.USE_LATEST_VERSION, true);
+        properties.put(AbstractKafkaSchemaSerDeConfig.AUTO_REGISTER_SCHEMAS, false);
+        return properties;
     }
 }
