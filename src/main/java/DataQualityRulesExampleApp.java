be425664e7a7f11687742b29240e733f5a1a2b40
gphilipp <gilles.philippart@gmail.com>
2023-10-24 15:18:16 +0200

    Inline props loading

--- src/main/java/DataQualityRulesExampleApp.java
+++ src/main/java/DataQualityRulesExampleApp.java
@@ -18,50 +18,41 @@ public class DataQualityRulesExampleApp {
 
     private static final String TOPIC = "memberships";
 
-
-    public static Properties loadConfig(final String configFile) throws IOException {
-        if (!Files.exists(Paths.get(configFile))) {
-            throw new IOException(configFile + " not found.");
-        }
-        final Properties cfg = new Properties();
-        try (InputStream inputStream = new FileInputStream(configFile)) {
-            cfg.load(inputStream);
-        }
-        return cfg;
-    }
-
     public static void main(String[] args) throws IOException {
         final Properties properties = loadProps();
-
-        try (KafkaProducer<String, Membership> producer = new KafkaProducer<>(properties)) {
-            Membership membership = Membership.newBuilder()
-                    .setEmail("john.doe")
-                    .setSsn("fizzbuzz")
-                    .setStartDate(LocalDate.of(2023, Month.JANUARY, 1))
-                    .setEndDate(LocalDate.of(1970, Month.JANUARY, 1))
-                    .build();
-            ProducerRecord<String, Membership> record = new ProducerRecord<>(TOPIC, membership);
-            producer.send(record, (recordMetadata, e) -> {
-                if (e == null) {
-                    System.out.println("Record written to partition " + recordMetadata.partition() +
-                            ", offset " + recordMetadata.offset() +
-                            ", timestamp " + recordMetadata.timestamp());
-                } else {
-                    e.printStackTrace(System.err);
-                }
-            });
-            producer.flush();
-        } catch (Exception e) {
-            System.err.println("Unable to produce, cause: " + e.getCause().getMessage());
-        }
+        KafkaProducer<String, Membership> producer = new KafkaProducer<>(properties);
+        Membership membership = Membership.newBuilder()
+                .setEmail("john.doe")
+                .setSsn("fizzbuzz")
+                .setStartDate(LocalDate.of(2023, Month.JANUARY, 1))
+                .setEndDate(LocalDate.of(1970, Month.JANUARY, 1))
+                .build();
+        ProducerRecord<String, Membership> record = new ProducerRecord<>(TOPIC, membership);
+        producer.send(record, (recordMetadata, e) -> {
+            if (e == null) {
+                System.out.println("Record written to partition " + recordMetadata.partition() +
+                        ", offset " + recordMetadata.offset() +
+                        ", timestamp " + recordMetadata.timestamp());
+            } else {
+                e.printStackTrace(System.err);
+            }
+        });
+        producer.flush();
     }
 
     private static Properties loadProps() throws IOException {
-        final Properties properties = loadConfig("client.properties");
-        properties.put(ProducerConfig.KEY_SERIALIZER_CLASS_CONFIG, StringSerializer.class);
-        properties.put(ProducerConfig.VALUE_SERIALIZER_CLASS_CONFIG, KafkaAvroSerializer.class);
-        properties.put(AbstractKafkaSchemaSerDeConfig.USE_LATEST_VERSION, true);
-        properties.put(AbstractKafkaSchemaSerDeConfig.AUTO_REGISTER_SCHEMAS, false);
-        return properties;
+        if (!Files.exists(Paths.get("client.properties"))) {
+            throw new IOException("client.properties" + " not found.");
+        }
+        final Properties props = new Properties();
+        try (InputStream inputStream = new FileInputStream("client.properties")) {
+            props.load(inputStream);
+        }
+        props.put(ProducerConfig.KEY_SERIALIZER_CLASS_CONFIG, StringSerializer.class);
+        props.put(ProducerConfig.VALUE_SERIALIZER_CLASS_CONFIG, KafkaAvroSerializer.class);
+        props.put(AbstractKafkaSchemaSerDeConfig.USE_LATEST_VERSION, true);
+        props.put(AbstractKafkaSchemaSerDeConfig.AUTO_REGISTER_SCHEMAS, false);
+        return props;
     }
+
 }
