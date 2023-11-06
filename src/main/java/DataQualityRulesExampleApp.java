24fcca68d40df4a8caa429639dbb707c1fd48ac5
gphilipp <gilles.philippart@gmail.com>
2023-11-06 08:46:19 +0100

    Set the producer properties in main

--- src/main/java/DataQualityRulesExampleApp.java
+++ src/main/java/DataQualityRulesExampleApp.java
@@ -16,29 +16,38 @@ import java.util.Properties;
 
 public class DataQualityRulesExampleApp {
 
-    public static void main(String[] args) throws IOException {
-        final Properties properties = loadProps();
-        KafkaProducer<String, Membership> producer = new KafkaProducer<>(properties);
+    public static void main(String[] args) throws IOException, InterruptedException {
+        final Properties props = loadClientProperties();
+        props.put(ProducerConfig.KEY_SERIALIZER_CLASS_CONFIG, StringSerializer.class);
+        props.put(ProducerConfig.VALUE_SERIALIZER_CLASS_CONFIG, KafkaAvroSerializer.class);
+        props.put(AbstractKafkaSchemaSerDeConfig.USE_LATEST_VERSION, true);
+        props.put(AbstractKafkaSchemaSerDeConfig.AUTO_REGISTER_SCHEMAS, false);
+        KafkaProducer<String, Membership> producer = new KafkaProducer<>(props);
         Membership membership = Membership.newBuilder()
-                .setEmail("john.doe")
-                .setSsn("fizzbuzz")
                 .setStartDate(LocalDate.of(2023, Month.JANUARY, 1))
                 .setEndDate(LocalDate.of(1970, Month.JANUARY, 1))
+                .setEmail("john.doe")
+                .setSsn("fizzbuzz")
                 .build();
         ProducerRecord<String, Membership> record = new ProducerRecord<>("memberships", membership);
-        producer.send(record, (recordMetadata, e) -> {
-            if (e == null) {
-                System.out.println("Record written to partition " + recordMetadata.partition() +
-                        ", offset " + recordMetadata.offset() +
-                        ", timestamp " + recordMetadata.timestamp());
-            } else {
-                e.printStackTrace(System.err);
-            }
-        });
-        producer.flush();
+        try {
+            producer.send(record, (recordMetadata, e) -> {
+                if (e == null) {
+                    System.out.println("Record written to partition " + recordMetadata.partition() +
+                            ", offset " + recordMetadata.offset() +
+                            ", timestamp " + recordMetadata.timestamp());
+                } else {
+                    e.printStackTrace(System.err);
+                }
+            });
+            producer.flush();
+        } catch (Exception e) {
+            Thread.sleep(10000);
+            System.err.println("Error");
+        }
     }
 
-    private static Properties loadProps() throws IOException {
+    private static Properties loadClientProperties() throws IOException {
         if (!Files.exists(Paths.get("client.properties"))) {
             throw new IOException("client.properties" + " not found.");
         }
@@ -46,10 +55,6 @@ public class DataQualityRulesExampleApp {
         try (InputStream inputStream = new FileInputStream("client.properties")) {
             props.load(inputStream);
         }
-        props.put(ProducerConfig.KEY_SERIALIZER_CLASS_CONFIG, StringSerializer.class);
-        props.put(ProducerConfig.VALUE_SERIALIZER_CLASS_CONFIG, KafkaAvroSerializer.class);
-        props.put(AbstractKafkaSchemaSerDeConfig.USE_LATEST_VERSION, true);
-        props.put(AbstractKafkaSchemaSerDeConfig.AUTO_REGISTER_SCHEMAS, false);
         return props;
     }
 
